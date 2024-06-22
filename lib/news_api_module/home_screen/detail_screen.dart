488d77d37c0ef8863eba27b429af66c2ee7de058
unvireak <dydy11v@gmail.com>
2024-06-22 23:11:28 +0700

    detail screen sticky app bar

--- lib/news_api_module/home_screen/detail_screen.dart
+++ lib/news_api_module/home_screen/detail_screen.dart
@@ -19,154 +19,177 @@ class DetailScreen extends StatefulWidget {
 class _DetailScreenState extends State<DetailScreen> {
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-    final screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: _buildBody(context, screenHeight, screenWidth),
+      body: _buildBody(context),
     );
   }
 
-  Widget _buildBody(BuildContext context, double screenHeight, double screenWidth) {
+  Widget _buildBody(BuildContext context) {
     List<Article> moreArticles = widget.articles.where((article) => article != widget.item).toList();
 
-    return SingleChildScrollView(
-      child: Stack(
-        children: [
-          Container(
-            height: screenHeight * 0.3,
-            child: Stack(
-              fit: StackFit.expand,
-              children: [
-                Image.network(
-                  widget.item.urlToImage.toString(),
-                  fit: BoxFit.cover,
+    return CustomScrollView(
+      slivers: [
+        SliverAppBar(
+          expandedHeight: 300.0,
+          pinned: true,
+          flexibleSpace: LayoutBuilder(
+            builder: (BuildContext context, BoxConstraints constraints) {
+              // Calculate opacity based on app bar's expanded height
+              var top = constraints.biggest.height;
+              bool isCollapsed = top <= kToolbarHeight + MediaQuery.of(context).padding.top;
+              return FlexibleSpaceBar(
+                title: isCollapsed
+                    ? Container(
+                      // color: Colors.red,
+                      margin: EdgeInsets.only(right: 65),
+                      child: Text(
+                          widget.item.title,
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20.0,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          maxLines: 1,
+                          softWrap: false,
+                          overflow: TextOverflow.fade,
+                        ),
+                    )
+                    : null,
+                background: Stack(
+                  fit: StackFit.expand,
+                  children: [
+                    Image.network(
+                      widget.item.urlToImage.toString(),
+                      fit: BoxFit.cover,
+                    ),
+                    Container(
+                      color: Colors.black45,
+                    ),
+                  ],
                 ),
-                SafeArea(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: Icon(Icons.arrow_back_ios, color: Colors.white),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(Icons.share, color: Colors.white),
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              );
+            },
           ),
-          Container(
-            padding: EdgeInsets.fromLTRB(8, screenHeight * 0.32, 8, 0),
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(8),
-                  child: Row(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(
-                            Icons.calendar_today_outlined,
+          leading: IconButton(
+            icon: Icon(Icons.arrow_back_ios),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+          actions: [
+            IconButton(
+              icon: Icon(Icons.share),
+              onPressed: () {
+                // Implement share functionality
+              },
+            ),
+          ],
+        ),
+        SliverList(
+          delegate: SliverChildListDelegate(
+            [
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Row(
+                  children: [
+                    Row(
+                      children: [
+                        Icon(
+                          Icons.calendar_today_outlined,
+                          color: Colors.grey,
+                          size: 15,
+                        ),
+                        SizedBox(width: 4),
+                        Text(
+                          DateFormat('yyyy-MM-dd').format(DateTime.parse(widget.item.publishedAt.toString())),
+                          style: TextStyle(
                             color: Colors.grey,
-                            size: 15,
+                            fontSize: 15,
                           ),
-                          SizedBox(width: 4),
-                          Text(
-                            DateFormat('yyyy-MM-dd').format(DateTime.parse(widget.item.publishedAt.toString())),
-                            style: TextStyle(
-                              color: Colors.grey,
-                              fontSize: 15,
-                            ),
-                          ),
-                        ],
-                      ),
-                      Spacer(),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.favorite_outline,
-                          color: Colors.grey,
                         ),
-                      ),
-                      IconButton(
-                        onPressed: () {},
-                        icon: Icon(
-                          Icons.bookmark_outline,
-                          color: Colors.grey,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  child: Text(
-                    widget.item.title,
-                    style: TextStyle(
-                      color: Colors.black,
-                      fontSize: 25,
-                      fontWeight: FontWeight.w700,
+                      ],
                     ),
-                    textAlign: TextAlign.left,
-                  ),
-                ),
-                Container(
-                  padding: EdgeInsets.all(8),
-                  child: Text(
-                    widget.item.description.toString(),
-                    style: TextStyle(
-                      fontSize: 18,
+                    Spacer(),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.favorite_outline,
+                        color: Colors.grey,
+                      ),
                     ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.bookmark_outline,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  widget.item.title,
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 25,
+                    fontWeight: FontWeight.w700,
+                  ),
+                  textAlign: TextAlign.left,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text(
+                  widget.item.description.toString(),
+                  style: TextStyle(
+                    fontSize: 18,
                   ),
                 ),
-                _buildMoreNews(moreArticles),
-              ],
-            ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(8.0),
+                child: Text('More News', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold)),
+              ),
+              _buildMoreNews(moreArticles),
+            ],
           ),
-        ],
-      ),
+        ),
+      ],
     );
   }
 
-Widget _buildMoreNews(List<Article> articles) {
-  return Container(
-    height: 400, // Adjust height as needed
-    child: SingleChildScrollView(
-      physics: NeverScrollableScrollPhysics(), // Disable scrolling
-      child: Column(
-        children: articles.map((article) => _buildItem(article)).toList(),
-      ),
-    ),
-  );
-}
-
-  Widget _buildItem(Article item) {
-  return InkWell(
-    onTap: () {
-      Navigator.of(context).push(
-        MaterialPageRoute(
-          builder: (context) => DetailScreen(
-            item: item,
-            articles: widget.articles.where((article) => article != item).toList(),
-          ),
-        ),
-      );
-    },
-    child: Card(
-      child: ListTile(
-        title: Text(item.title.toString()),
-        subtitle: Text(item.content.toString()),
-      ),
-    ),
-  );
-}
-  
+  Widget _buildMoreNews(List<Article> articles) {
+    return ListView.builder(
+      physics: NeverScrollableScrollPhysics(),
+      shrinkWrap: true,
+      itemCount: articles.length,
+      itemBuilder: (context, index) {
+        return _buildItem(articles[index]);
+      },
+    );
   }
 
+  Widget _buildItem(Article item) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              item: item,
+              articles: widget.articles.where((article) => article != item).toList(),
+            ),
+          ),
+        );
+      },
+      child: Card(
+        child: ListTile(
+          leading: item.urlToImage.toString() != "null" ? Image.network(item.urlToImage.toString()) : SizedBox(),
+          title: Text(item.title),
+          subtitle: Text(item.content.toString()),
+        ),
+      ),
+    );
+  }
+}
